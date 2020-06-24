eaabde57b6c8372d9e8137523e176707e9dc359f
abdulkatherjava <abdulkather2025@gmail.com>
2020-06-24 22:05:34 +0530

    Jokes_APP

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import React from 'react';
-import ContactCard from "./ContactCard"
+import React from "react"
+import Joke from "./Joke"
+import jokesData from "./JokesData"
 
 function App() {
+
+  console.log(jokesData);
+  // const jokeComponents = jokesData.map(joke=> <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
+
+  const jokeComponents = jokesData.map(joke => <Joke key={joke.id} question={joke.question} punchLine={joke.punchLine} />)
+
+  console.log("hai")
   return (
-    <div className="contacts">
-      <ContactCard contact= {{name: "Abdul Kather",
-            imgUrl: "https://picsum.photos/200",
-            mobileno: "994057887",
-            address: "no 100, main road, Chennai" }} />
-      <hr/>  
-      <ContactCard contact= {{name: "Jony",
-            imgUrl: "https://picsum.photos/100",
-            mobileno: "994057887",
-            address: "no 200, south road, Delhi" }} />
-      <hr/>
-      <ContactCard contact= {{name: "Amigose",
-            imgUrl: "https://picsum.photos/400",
-            mobileno: "9283749283",
-            address: "no 300, west road, Bombay" }} />
-      <hr/>
-      <ContactCard contact= {{name: "Scrimba",
-            imgUrl: "https://picsum.photos/500",
-            mobileno: "893434982",
-            address: "no 400, East road, Kolkatta" }} />
+    <div>
+      {jokeComponents}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/JokesData.js
@@ -0,0 +1,29 @@
+const JokesData = [
+    {
+       "id":1,
+       "question":"When does a Joke become a Dad Joke?",
+       "punchLine":"When the punchline becomes apparent"
+    },
+    {
+       "id":2,
+       "question":"The greatest Schrodinger punchline...",
+       "punchLine":"or maybe not."
+    },
+    {
+       "id":3,
+       "question":"You put the punch line in the title",
+       "punchLine":"How do you ruin a good joke on Reddit?"
+    },
+    {
+       "id":4,
+       "question":"Punch lines are extremely one-dimensional",
+       "punchLine":"Punch areas and punch volumes have more depth."
+    },
+    {
+       "id":5,
+       "question":"",
+       "punchLine":"If you sit down to enjoy a hot cup of coffee, then your boss will ask you to do something that will last until the coffee is cold."
+    }
+ ];
+
+ export default JokesData;
